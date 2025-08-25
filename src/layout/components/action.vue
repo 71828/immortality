28c3efaed5b170cc426e4d4ce79a5cbeda0b5da2
qianchao <1343404376@qq.com>
2025-08-26 00:21:30 +0800

    1

--- src/layout/components/action.vue
+++ src/layout/components/action.vue
@@ -12,24 +12,25 @@ function toAction(item) {
 }
 
 </script>
-<template> 
+<template>
     <div class="">
 
         <div class="aciton-list">
             <template v-for="(item, index) in actionList" :key="index">
-                <div class="item" :class="{isExecution:item.isExecution,expand:item.expand}"  @click="toAction(item)">
-               
+                <div class="item" :class="{ isExecution: item.isExecution, expand: item.expand }" @click="toAction(item)">
+
                     <div class="head">
                         <div class="name">{{ item.name }} </div>
-                        <div>{{ item.Proficiency.level }} 
+                        <div>{{ item.Proficiency.level }}
                             <span v-if="item.Proficiency.maxLevel">/{{ item.Proficiency.maxLevel }} </span>
                         </div>
                     </div>
-                    <div class="content" >
+                    <div class="content">
                         <div>
-                           熟练度: <span> {{ item.Proficiency.val === 0 ? 0 : item.Proficiency.val.toFixed(1) }}/{{item.Proficiency.capacity }}</span>
+                            熟练度: <span> {{ item.Proficiency.val === 0 ? 0 : item.Proficiency.val.toFixed(1)
+                                }}/{{ item.Proficiency.capacity.toFixed(1) }}</span>
                         </div>
-                        <div class="perScond" v-if="item.isExecution">+{{ item.Proficiency.perScond  }}</div>
+                        <div class="perSecond" v-if="item.isExecution">+{{ item.Proficiency.perSecond }}</div>
                     </div>
                     <div class="progress">
                         <div class="bar-wrap">
@@ -41,19 +42,25 @@ function toAction(item) {
                     </div>
                     <div class="foot">
                         <div>
-                             <el-icon class="icon" @click.stop="item.expand=!item.expand"> <MoreFilled /></el-icon>
+                            <el-icon class="icon" @click.stop="item.expand = !item.expand">
+                                <MoreFilled />
+                            </el-icon>
                         </div>
-                        <el-icon class="icon" v-if="item.isExecution" > <VideoPause /></el-icon>
-                        <el-icon class="icon" v-else ><VideoPlay /></el-icon>
+                        <el-icon class="icon" v-if="item.isExecution">
+                            <VideoPause />
+                        </el-icon>
+                        <el-icon class="icon" v-else>
+                            <VideoPlay />
+                        </el-icon>
                     </div>
                     <div class="desc" v-show="item.expand">
                         <div class="cell">
-                            <div class="label">消耗   <span class="mark1">精力 </span> 每秒 </div>
+                            <div class="label">消耗 <span class="mark1">精力 </span> 每秒 -1 </div>
                             <div class="value"></div>
                         </div>
                         <div class="dvi"></div>
                         <div class="cell">
-                            <div class="label">完成此动作提升   <span class="mark">气血 </span> +1 </div>
+                            <div class="label">完成此动作提升 <span class="mark">气血 </span> +1 </div>
                             <div class="value"></div>
                         </div>
                     </div>
@@ -64,21 +71,23 @@ function toAction(item) {
 </template>
 <style lang="scss" scoped>
 .aciton-list {
-    
+
     min-height: 500px;
     overflow: visible;
+
     .item {
         background-color: #171b23;
         width: 200px;
         color: #ccc;
-        padding: 4px 12px;
+        padding: 8px 12px;
         border-radius: 16px;
         box-shadow: 0px 0px 20px 10px #171b2304;
         cursor: pointer;
-        border: 2px solid #101118;
+        border: 2px solid #171b23;
         transition: .3s;
         position: relative;
-        .desc{
+
+        .desc {
             position: absolute;
             left: -2px;
             right: -2px;
@@ -92,41 +101,42 @@ function toAction(item) {
             font-size: 14px;
             padding: 12px;
             line-height: 21px;
-               border-left: 2px solid #171b23;
-                    border-right: 2px solid #171b23;
-                    border-bottom: 2px solid #171b23;
-            .dvi{
+            border-left: 2px solid #171b23;
+            border-right: 2px solid #171b23;
+            border-bottom: 2px solid #171b23;
+            .dvi {
                 border-bottom: 1px solid #23252f;
             }
-            .mark{
+            .mark {
                 color: #fc263f;
             }
-            .mark1{
+            .mark1 {
                 color: #ffb730;
             }
         }
-        &.isExecution{
+
+        &.isExecution {
             border: 2px solid #7d96f148;
-           
-                .desc{
-                    border-left: 2px solid #7d96f148;
-                    border-right: 2px solid #7d96f148;
-                    border-bottom: 2px solid #7d96f148;
-                }
-            
+            .desc {
+                border-left: 2px solid #7d96f148;
+                border-right: 2px solid #7d96f148;
+                border-bottom: 2px solid #7d96f148;
+            }
         }
-        &:hover{
+        &:hover {
             border: 2px solid #7a96fa45;
-               .desc{
-                    border-left: 2px solid #7d96f148;
-                    border-right: 2px solid #7d96f148;
-                    border-bottom: 2px solid #7d96f148;
-                }
+            .desc {
+                border-left: 2px solid #7d96f148;
+                border-right: 2px solid #7d96f148;
+                border-bottom: 2px solid #7d96f148;
+            }
         }
-        &.expand{
-                border-bottom-left-radius: 0px;
-                border-bottom-right-radius: 0px;
-            .desc{
+
+        &.expand {
+            border-bottom-left-radius: 0px;
+            border-bottom-right-radius: 0px;
+
+            .desc {
                 border-top-left-radius: 0px;
                 border-top-right-radius: 0px;
                 border-bottom-left-radius: 16px;
@@ -134,37 +144,46 @@ function toAction(item) {
                 height: 100px;
             }
         }
+
         .head {
             display: flex;
             justify-content: space-between;
             margin-bottom: 12px;
             font-size: 16px;
-            .name{
-                font-size: 20px;
+            align-items: center;
+
+            .name {
+                font-size: 16px;
+                font-weight: 600;
             }
         }
-        .content{
+
+        .content {
             margin-bottom: 4px;
             display: flex;
             justify-content: space-between;
-            .perScond{
+            font-size: 14px;
+
+            .perSecond {
                 color: #5378f9;
             }
         }
-        .foot{
+
+        .foot {
             padding-top: 8px;
             display: flex;
             justify-content: space-between;
             font-size: 14px;
-            
-            .icon{
+
+            .icon {
                 font-size: 26px;
                 padding: 2px;
-                &:hover{
+
+                &:hover {
                     color: #fff;
                     font-weight: 700;
                     background-color: #101118;
-                    
+
                     border-radius: 12px;
                 }
             }
@@ -190,6 +209,7 @@ function toAction(item) {
             top: 0;
             left: 0;
             background: linear-gradient(90deg, #1d1da3 0%, #4242cf 60%, #5a81ff 100%);
+            border-radius: 6px;
         }
     }
 
--- src/view/main.vue
+++ src/view/main.vue
@@ -5,56 +5,69 @@ import { useMainStore } from '@/store/store'
 import { playAttribute } from '@/store/playAttribute'
 import { executionList } from '@/store/playAction'
 
-const playAttr =playAttribute()
+const playAttr = playAttribute()
 import layout from '../layout/index.vue'
 const mainDate = useMainStore();
-    const intervalId = ref(null);
-    function init() {
-      intervalId.value = setInterval(() => {
-        mainDate.increment()
-        mainLoop()
-      }, 10);
-    }
+const intervalId = ref(null);
+function init() {
+  intervalId.value = setInterval(() => {
+    mainDate.increment()
+    mainLoop()
+  }, 10);
+}
 
 
 function mainLoop() {
+  execution()
+  regeneration()
+}
+function regeneration() {
+  const list = ['QB','EP','MP',]
+  list.map(m=>{
+    if (playAttr[m].val<playAttr[m].max) {
+      const val = playAttr[m].perSecond / 100      
+       playAttr[m].val=((playAttr[m].val+val)<playAttr[m].max)?playAttr[m].val+val:playAttr[m].max
+    }
+  })
+}
+function execution() {
   if (executionList().list.length) {
-    const item=executionList().list[0]
-    let {level,val,perScond,capacity,levelRate, maxLevel,} =item.Proficiency
-    const {frameChanges,levelChanges}=item
-    val+=perScond/100
+    const item = executionList().list[0]
+    let { level, val, perSecond, capacity, levelRate, maxLevel, } = item.Proficiency
+    const { frameChanges, levelChanges } = item
+    val += perSecond / 100
 
-    if (val<capacity) {
-       item.Proficiency.val= Number(val);
-        if (frameChanges.length) {
-          frameChanges.map(m=>{
-            playAttr[m.attrTarget][m.keyTarget]+=m.perScond/100
-          })
-        }
-    }else{
-      if(level<maxLevel||maxLevel===0){
-        item.Proficiency.val= Number(val)-capacity;
-        item.Proficiency.capacity=((1+levelRate)*capacity).toFixed(0)
-        item.Proficiency.level+=1
+    if (val < capacity) {
+      item.Proficiency.val = Number(val);
+      if (frameChanges.length) {
+        frameChanges.map(m => {
+
+          
+          playAttr[m.attrTarget][m.keyTarget] = (playAttr[m.attrTarget][m.keyTarget]*100+m.perSecond )/100
+        })
+      }
+    } else {
+      if (level < maxLevel || maxLevel === 0) {
+        item.Proficiency.val = Number(val) - capacity;
+        item.Proficiency.capacity = ((1 + levelRate) * capacity)
+        item.Proficiency.level += 1
       }
       if (levelChanges.length) {
-          levelChanges.map(m=>{
-            console.log(m);
-            
-            playAttr[m.attrTarget][m.keyTarget]+=m.perLevel
-          })
+        levelChanges.map(m => {
+          playAttr[m.attrTarget][m.keyTarget] += m.perLevel
+        })
       }
-
     }
+
+
+
   }
 }
-
- 
 onMounted(() => {
   init()
 
 });
- 
+
 onBeforeUnmount(() => {
   if (intervalId.value) {
     clearInterval(intervalId.value);
@@ -65,12 +78,10 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
- <div>
-    <layout/>
-    
- </div>
+  <div>
+    <layout />
+
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
